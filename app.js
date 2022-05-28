363f0d12e5be5ad57948bd685ea138c1fb3f63b8
INDRASEN <indrasenkumar715@gmail.com>
2022-05-28 12:18:33 +0530

    Fixed port issue\

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser');
+require('dotenv').config();
+require('./db/database')
 const app = express();
 const authRouter = require('./api/routes/User')
-const database = require('./db/database')
 const apiCredentialsMiddleware = require('./api/middleware/api-credentials')
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -13,6 +14,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api/v1/auth/', apiCredentialsMiddleware, authRouter)
 
 
-app.listen(port=>{
-    console.log("server is runing on port"+port)
+app.listen(port,()=>{
+    console.log("Your API Server is runing on port: "+port)
 })
