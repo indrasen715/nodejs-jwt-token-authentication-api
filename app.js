81fcbb7605c9ad011f0ec04bd74f5e50851b920b
INDRASEN <indrasenkumar715@gmail.com>
2022-05-29 05:57:55 +0530

    updated code

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 require('dotenv').config();
+
 require('./db/database')
 const app = express();
 const authRouter = require('./api/routes/User')
